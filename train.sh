b2717832fd7b1c7697ffd8fa84f7ff4ce8e2bc53
Kurtulus Birgoren <kbirgoren@gmail.com>
2023-01-02 22:20:48 +0100

    Updates

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5"
 pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse"
-output_dir="models/malikurtulus" 
+output_dir="models/kurtulusbirgoren" 
+save_sample_prompt="photo of kurtulusbirgoren"
 
 accelerate launch train_dreambooth.py \
 	--pretrained_model_name_or_path=$pretrained_model_name_or_path \
@@ -13,7 +14,6 @@ accelerate launch train_dreambooth.py \
 	--train_batch_size=1 \
 	--train_text_encoder \
 	--mixed_precision="fp16" \
-	--use_8bit_adam \
 	--gradient_accumulation_steps=1 \
 	--learning_rate=1e-6 \
 	--lr_scheduler="constant" \
@@ -22,4 +22,5 @@ accelerate launch train_dreambooth.py \
 	--sample_batch_size=4 \
 	--max_train_steps=1100 \
 	--save_interval=10000 \
+	--save_sample_prompt="$save_sample_prompt" \
 	--concepts_list="concepts_list.json"
